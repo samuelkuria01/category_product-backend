75cc1a2f5954708c06f152b9cb336178beef33f6
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2024-01-24 01:19:33 +0300

    debugging the categroy table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_23_115126) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_23_213621) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_23_115126) do
   end
 
   create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "category", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
