56b00d0f1f1f3377bb0139fa85da2694e431d0fe
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2024-01-24 08:13:06 +0300

    debug 06

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,21 +3,31 @@ class CategoriesController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
     before_action :authorize_admin, only: [:new, :create, :edit, :update, :destroy]
   
+
+    def index
+      @categories = Category.all
+      render json: @categories, only: [:name]
+    rescue ActiveRecord::StatementInvalid => e
+      render json: { error: 'Categories table does not exist' }, status: :internal_server_error
+    end
+    
     # def index
     #   @categories = Category.all
     #   @user_email = current_user.email if user_signed_in?
   
     #   render json: @categories, only: [:name ]
     # end
-    def index
-      if Category.table_exists?
-        @categories = Category.all
-        # @user_email = current_user.email if user_signed_in?
-        render json: @categories, only: [:name]
-      else
-        render json: { error: 'Categories table does not exist' }, status: :internal_server_error
-      end
-    end
+    # def index
+    #   if Category.table_exists?
+    #     @categories = Category.all
+    #     # @user_email = current_user.email if user_signed_in?
+    #     render json: @categories, only: [:name]
+    #   else
+    #     render json: { error: 'Categories table does not exist' }, status: :internal_server_error
+    #   end
+    # end
+
+
     
   
     # GET /categories/1 or /categories/1.json
